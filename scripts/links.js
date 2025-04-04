554f3ce90caf3d5bfe5d726a0b8fa7491ea34555
Tom Seidel <hi@tomseidel.com>
2025-04-04 18:59:17 +0200

    fix: share and close buttons on safari mobile

--- scripts/links.js
+++ scripts/links.js
@@ -27,14 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// ----- Share Modal Functionality -----
-const shareButton = document.getElementById("share-button");
+// ----- Menu Modal Functionality -----
+const menuButton = document.getElementById("menu-button");
 const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal-frame");
 
-shareButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-icon lucide-ellipsis"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>`
+menuButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-icon lucide-ellipsis"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>`
 
-shareButton.addEventListener("click", () => {
+menuButton.addEventListener("click", () => {
     overlay.classList.add("active"); // Show
     modal.classList.add("active");
 });
